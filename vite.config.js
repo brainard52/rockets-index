c02d91bffa9c696694a0534507b951b9346bef29
Landon R. Brainard <landon@redshyft.com>
2024-01-16 03:51:18 -0800

    Updated links for new file locations and rebuilt via vite

--- vite.config.js
+++ vite.config.js
@@ -11,8 +11,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
-        search: resolve(__dirname, 'src/search/index.html'),
-        list: resolve(__dirname, 'src/list/index.html'),
       }
     }
   }
